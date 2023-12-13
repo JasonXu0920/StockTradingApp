a34654d11a5aaa93de9dfc3855a6fb0e63a0c384
Jason Xu <97411709+JasonXu0920@users.noreply.github.com>
2023-12-13 20:54:32 +1100

    schduling data fecthing

--- pupulate_db.py
+++ pupulate_db.py
@@ -6,14 +6,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 connection = sqlite3.connect("app.db")
+connection.row_factory = sqlite3.Row
 cursor = connection.cursor()
+
+cursor.execute("""
+    SELECT symbol, company FROM stock
+""")
+
+rows = cursor.fetchall()
+symbols = [row['symbol'] for row in rows]
 
 api = tradeapi.REST(os.getenv('API_KEY'), os.getenv('API_SECRETE'), base_url='https://paper-api.alpaca.markets')
 assets = api.list_assets()
 
 for asset in assets:
     try:
-        if asset.status == 'active':
+        if asset.status == 'active' and asset.tradable and asset.symbol not in symbols:
             cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
     except Exception as e:
         print(e)
