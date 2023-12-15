dbdcc6afa7d8327aec3dee47855409cf44981dd8
Jason Xu <97411709+JasonXu0920@users.noreply.github.com>
2023-12-16 09:38:50 +1100

    populate the price table

--- populate_price.py
+++ populate_price.py
@@ -20,6 +20,8 @@ for row in rows:
     symbols.append(symbol)
     stock_dict[symbol] = row['id']
 
+
+
 api = tradeapi.REST(config.API_KEY, config.API_SECRETE, base_url=config.BASE_URL)
 
 chunk_size = 200
@@ -27,14 +29,12 @@ for i in range(0, len(symbols), chunk_size):
     symbol_chunk = symbols[i:i+chunk_size]
     barsets = api.get_bars(symbol_chunk, TimeFrame(1, TimeFrameUnit.Day), "2023-06-01", "2023-12-01", adjustment='raw').df
 
-
-    for symbol in barsets:
-        print(f"processing symbol {symbol}")
-        for bar in barsets[symbol]:
-            stock_id = stock_dict[symbol]
-            cursor.execute("""
-                INSERT INTO stock price (stock_id, date, open, high, low, close, volume)
+    for index, row in barsets.iterrows():
+        print(f"Processing ${row['symbol']}")
+        stock_id = stock_dict[row['symbol']]
+        cursor.execute("""
+                INSERT INTO stock_price (stock_id, date, open, high, low, close, volume)
                 VALUES (?, ?, ?, ?, ?, ?, ?)
-            """, (stock_id, bar.t.date(), bar.o, bar.h, bar.l, bar.c, bar.v))
-
+        """, (stock_id, index.date(), row['open'], row['high'], row['low'], row['close'], row['volume']))
+        
 connection.commit()
